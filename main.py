76ed8c582e56514f15561d934b67dc91b7f17ed4
Satender Kundu <satenderkundu@hotmail.com>
2021-04-27 12:32:26 +0100

    removing non OOP code

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import pygame
 from pygame.locals import *
-#import time
+import time
 
 
-""" class Snake:
+class Snake:
     def __init__(self, parent_screen):
         self.parent_screen = parent_screen
         self.block = pygame.image.load("/Users/satenderkundu/Coding/pythonSnakeGame/resources/block.jpg").convert()
@@ -63,53 +63,9 @@ class Game:
 
                 elif event.type == QUIT:
                     running = False
-            self.snake.slither """
+            #self.snake.slither
 
-def draw_block():
-    surface.fill((250,105,55))
-    surface.blit(block, (block_x, block_y))
-    pygame.display.flip()
 
 if __name__ == "__main__":
-#   game = Game()
-#    game.run()
-
-
-    pygame.init()
-
-    surface = pygame.display.set_mode((700,500))
-    surface.fill((250,105,55))
-
-    block = pygame.image.load("/Users/satenderkundu/Coding/pythonSnakeGame/resources/block.jpg").convert()
-    block_x = 100
-    block_y = 100
-    surface.blit(block, (block_x,block_y))
-
-    pygame.display.flip()
-
-    running = True
-    
-    while running:
-        for event in pygame.event.get():
-            if event.type == KEYDOWN:
-                if event.key == K_ESCAPE:
-                    running = False
-
-                if event.key == K_UP:
-                    block_y -= 10
-                    draw_block()
-
-                if event.key == K_DOWN:
-                    block_y +=10
-                    draw_block()
-
-                if event.key == K_LEFT:
-                    block_x -=10
-                    draw_block()    
-
-                if event.key == K_RIGHT:
-                    block_x +=10
-                    draw_block()
-                    
-            elif event.type == QUIT:
-                running = False
+    game = Game()
+    game.run()
