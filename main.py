19b4f63963913dd71a63f35c0b07d927981bf7e0
Satender Kundu <satenderkundu@hotmail.com>
2021-05-02 01:08:39 +0100

    cnsistnt snk dirctnl mvmnt ovr lngth

--- main.py
+++ main.py
@@ -2,18 +2,21 @@ import pygame
 from pygame.locals import *
 import time
 
+SIZE = 40
 
 class Snake:
-    def __init__(self, parent_screen):
+    def __init__(self, parent_screen, length):
+        self.length = length
         self.parent_screen = parent_screen
         self.block = pygame.image.load("/Users/satenderkundu/Coding/pythonSnakeGame/resources/block.jpg").convert()
-        self.x = 100
-        self.y = 100
+        self.x = [SIZE]*length
+        self.y = [SIZE]*length
         self.direction = 'down'
 
     def draw(self):
         self.parent_screen.fill((250,105,55))
-        self.parent_screen.blit(self.block, (self.x, self.y))
+        for i in range(self.length):
+            self.parent_screen.blit(self.block, (self.x[i], self.y[i]))
         pygame.display.flip()
     
     def move_up(self):
@@ -29,14 +32,19 @@ class Snake:
         self.direction = 'right'
 
     def slither(self):
+
+        for i in range(self.length-1,0,-1):
+            self.x[i] = self.x[i - 1]
+            self.y[i] = self.y[i - 1]
+
         if self.direction == 'up':
-            self.y -=10
+            self.y[0] -=10
         if self.direction == 'down':
-            self.y +=10
+            self.y[0] +=10
         if self.direction == 'left':
-            self.x -=10
+            self.x[0] -=10
         if self.direction == 'right':
-            self.x +=10   
+            self.x[0] +=10   
 
         self.draw()
 
@@ -45,7 +53,7 @@ class Game:
         pygame.init()
         self.surface = pygame.display.set_mode((500, 700))
         self.surface.fill((250,105,55))
-        self.snake = Snake(self.surface)
+        self.snake = Snake(self.surface, 2)
         self.snake.draw()
 
     def run(self):
